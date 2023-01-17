a4ea773f420397d1362c3bbd360433a7390d530d
BorisMesh <boris.mescher@gmail.com>
2023-01-17 19:58:38 +0300

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App/App.jsx';
 import reportWebVitals from './reportWebVitals';
 
-
+console.log('tets');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <React.StrictMode>
